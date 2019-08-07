a86e6f378400d4315f098c6c186582cbdb8a8534
ValentinaY <valyatev@gmail.com>
2019-08-06 23:23:25 -0500

    Updated

--- Metodos/bisection.py
+++ Metodos/bisection.py
@@ -1,15 +1,11 @@
-#Función para polinomios
-def poli(x):
-      y=pow(x,2)
-      return ()
-#Programa principal
-print "Este programa encuentra una raíz, por el método de bisección"
-float(raw_input('Introduce el valor de xi'))
-float(raw_input('Introduce el valor de xs'))
-error=float((raw_input('Introduce el error')))
+def bisection(f, a, b, e):
+	c=(a+b)/2
+	while abs(f(c) - 0) > e: 
+		if f(a)*f(c) <0:
+			b=c
+		else:
+			a=c
+		c=(a+b)/2
+		print(c)
 
-while abs( ) > error:
-      xa = (xi+xs)/2.0
-if poli(xi)*poli(xa) < 0:
-      else:
-print"La raíz es: ",xa
+bisection( lambda x: pow(x-1.8974,3),0,2, 0.0000000001)
